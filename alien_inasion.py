b06cda88301e0181e7c51d064627050bb9af7980
azakg <azakgzuu@gmail.com>
2025-03-01 18:35:56 -0500

    add background

--- alien_inasion.py
+++ alien_inasion.py
@@ -4,9 +4,12 @@ import pygame
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         while True:
@@ -14,7 +17,11 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
+
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
